4575cdba8de6a5c26b5bd6f8720388c675ea243f
ss4774 <ss4774@student.uni-lj.si>
2021-11-05 15:18:19 +0100

    Added picking example

---
+++ examples/pickingExample/main.js
@@ -0,0 +1,108 @@
+import * as RC from "../../src/RenderCore.js";
+
+
+const canvas = new RC.Canvas(document.body);
+
+const renderer = new RC.MeshRenderer(canvas, RC.WEBGL2);
+renderer.clearColor = "#000000FF";
+renderer.addShaderLoaderUrls("../../src/shaders");
+
+const scene = new RC.Scene();
+const camera = new RC.PerspectiveCamera(75, canvas.width/canvas.height, 0.125, 128);
+camera.position.set(0, 2, 16);
+camera.lookAt(new RC.Vector3(0, 0, 0), new RC.Vector3(0, 1, 0));
+
+const pLight = new RC.PointLight(new RC.Color("#FDB813"), 1.0);
+scene.add(pLight);
+
+let text2D;
+populateScene();
+
+
+const renderQueue = new RC.RenderQueue(renderer);
+const PICK = new RC.PickerFX(
+    renderer, 
+    {scene: scene, camera: camera},
+    {},
+    new RC.FX.output("color_picker")
+);
+renderQueue.pushRenderQueue(PICK);
+
+const RenderPass_MainPass = new RC.RenderPass(
+    // Rendering pass type
+    RC.RenderPass.BASIC,
+    // Initialize function
+    (textureMap, additionalData) => { },
+    // Preprocess function
+    (textureMap, additionalData) => { return { scene: scene, camera: camera }; },
+    // PostprocesWs function
+    (textureMap, additionalData) => { },
+    // Target
+    RC.RenderPass.SCREEN,
+    // Viewport
+    { width: canvas.width, height: canvas.height },
+    // Bind depth texture to this ID
+    "depth_main",
+    // Outputs
+    [{id: "color_main", textureConfig: RC.RenderPass.DEFAULT_RGBA_TEXTURE_CONFIG}]
+);
+renderQueue.pushRenderPass(RenderPass_MainPass);
+
+
+function populateScene(){
+    for(let i = 0; i < 32; i++){
+        const cube = new RC.Cube(1.9, new RC.Color());
+        cube.material = new RC.MeshBasicMaterial();
+        cube.material.emissive.set(0.0, 0.0, 0.0);
+        cube.position.set((Math.random() * 16) - 8, (Math.random() * 16) - 8, (Math.random() * 8) - 4);
+        cube.colorID = cube.material.color;
+    
+        scene.add(cube);
+    }
+
+    const fontImgLoader = new RC.ImageLoader();
+    fontImgLoader.load("../common/textures/fonts/font2.png", function (image) {
+        const fontTexture = new RC.Texture(
+            image, 
+            RC.Texture.ClampToEdgeWrapping, 
+            RC.Texture.ClampToEdgeWrapping,
+            RC.Texture.NearestFilter, 
+            RC.Texture.NearestFilter,
+            RC.Texture.RGBA, 
+            RC.Texture.RGBA, 
+            RC.Texture.UNSIGNED_BYTE,
+            128,
+            256
+        );
+        fontTexture._generateMipmaps = false;
+
+        text2D = new RC.Text2D({text: "Click on objects.", fontTexture: fontTexture, xPos: 10, yPos: 10, fontSize: 32});
+        text2D.material.transparent = true;
+        scene.add(text2D);
+    });
+}
+
+function resizeFunction() {
+    canvas.updateSize();
+    renderer.updateViewport(canvas.width, canvas.height);
+};
+function mousedownFunction(event) {
+    console.log(event);
+
+    if (event.which === 1) {
+        const pickedColor = renderQueue.pickColor("color_picker", event.clientX, event.clientY);
+        console.log(pickedColor);
+        text2D.text = "R:" + ("00" + pickedColor[0]).slice(-3) + " G:" + ("00" + pickedColor[1]).slice(-3) + " B:" + ("00" + pickedColor[2]).slice(-3);
+    }
+}
+
+function renderFunction() {
+    renderQueue.render(scene, camera);
+    window.requestAnimationFrame(renderFunction);
+}
+
+window.onload = function() {
+    window.addEventListener("resize", resizeFunction, false);
+    window.addEventListener("mousedown", mousedownFunction, false);
+    window.requestAnimationFrame(renderFunction);
+};
